0646a25a3f4c9341115389f0b2857c36cf3b50f1
unikounio <bubbbble428@gmail.com>
2023-05-18 16:41:13 +0900

    -aオプションに関する記述を削除し、-rオプションに関する記述を追加 - -aオプションの定義に関する記述を-rに置き換えた - process_directory_entriesメソッドを書き換えた   - -rオプションがオンの場合は通常処理にArray#reverseを加える   - 通常処理を条件式の外に出す（重複回避）   - -aオプションに関する記述を削除   - 内容変更に伴いメソッド名をfilter_directory_entriesに変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -7,18 +7,15 @@ WIDTH = 18
 
 opt = OptionParser.new
 option = []
-opt.on('-a') { option << '-a' } # 今後のオプション追加に備えて、['-a']の代入ではなく空配列への追加という形をとっている。
+opt.on('-r') { option << '-r' } # 今後のオプション追加に備えて、['-r']の代入ではなく空配列への追加という形をとっている。
 opt.parse!(ARGV)
 
 argument = ARGV[0] || '.'
 
-def process_directory_entries(option, argument)
-  if option.include? '-a'
-    Dir.entries(argument).sort
-  else
-    raw_entries = Dir.entries(argument).sort
-    raw_entries.reject { |entry| entry.start_with? '.' }
-  end
+def filter_directory_entries(option, argument)
+  raw_entries = Dir.entries(argument).sort
+  filtered_entries = raw_entries.reject { |entry| entry.start_with? '.' }
+  option.include?('-r') ? filtered_entries.reverse : filtered_entries
 end
 
 def hankaku_ljust(string, width, padding = ' ')
@@ -27,7 +24,7 @@ def hankaku_ljust(string, width, padding = ' ')
 end
 
 if File.directory? argument
-  entries = process_directory_entries(option, argument)
+  entries = filter_directory_entries(option, argument)
 elsif File.file? argument
   puts ARGV[0]
 else

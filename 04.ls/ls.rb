9117a0fb1daa222136bc05dc5f279f46040a7bbe
unikounio <bubbbble428@gmail.com>
2023-05-27 04:12:46 +0900

    File::Stat#modeの表示を変換するメソッドを追加 - File::Stat#modeの8進数をファイル名とアクセス権を示すアルファベットまたは記号に変換するconvert_modeメソッドを追加   - ファイル名に関する処理を行うconvert_filetypeメソッドを追加   - アクセス権に関する処理を行うconvert_rightメソッドを追加     - 所有者のアクセス権に関する処理を行うconvert_user_rightメソッドを追加     - グループのアクセス権に関する処理を行うconvert_group_rightメソッドを追加     - その他のアクセス権に関する処理を行うconvert_etc_rightメソッドを追加     - 特殊権限に関する処理を行うconvert_special_rightメソッドを追加

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -48,7 +48,7 @@ def list(argument)
   end
 
   entries_path.each do |entry_path|
-    print "#{analyze_mode(entry_path)} "
+    print "#{convert_mode(entry_path)} "
     print "#{File.lstat(entry_path).nlink} "
     print "#{File.lstat(entry_path).uid} "
     print "#{File.lstat(entry_path).gid} "
@@ -58,8 +58,79 @@ def list(argument)
   end
 end
 
-def analyze_mode(entry)
-  File.lstat(entry).mode
+def convert_mode(entry_path)
+  mode = File.lstat(entry_path).mode.to_s(8)
+  mode = convert_filetype(mode)
+  mode = convert_right(mode)
+  mode
+end
+
+def convert_filetype(mode)
+  mode = mode.rjust(6, '0')
+  mode = mode.gsub(/^01/, 'p')
+  mode = mode.gsub(/^02/, 'c')
+  mode = mode.gsub(/^04/, 'd')
+  mode = mode.gsub(/^06/, 'b')
+  mode = mode.gsub(/^10/, '-')
+  mode = mode.gsub(/^12/, 'l')
+  mode = mode.gsub(/^14/, 's')
+  mode
+end
+
+def convert_right(mode)
+  mode = convert_user_right(mode)
+  mode = convert_group_right(mode)
+  mode = convert_etc_right(mode)
+  mode = convert_special_right(mode)
+  mode
+end
+
+def convert_special_right(mode)
+  if /^(.)1/ =~ mode
+    mode = mode.gsub(/-$/, 'T')
+    mode = mode.gsub(/x$/, 't')
+  elsif /^(.)2/ =~ mode || /^(.)3/ =~ mode
+    mode = mode.gsub(/-$/, 'S')
+    mode = mode.gsub(/x$/, 's')
+  end
+
+  mode = mode.gsub(/^(.)\d/, '\1')
+  mode
+end
+
+def convert_user_right(mode)
+  mode = mode.gsub(/0(..)$/, '---\1')
+  mode = mode.gsub(/1(..)$/, '--x\1')
+  mode = mode.gsub(/2(..)$/, '-w-\1')
+  mode = mode.gsub(/3(..)$/, '-wx\1')
+  mode = mode.gsub(/4(..)$/, 'r--\1')
+  mode = mode.gsub(/5(..)$/, 'r-x\1')
+  mode = mode.gsub(/6(..)$/, 'rw-\1')
+  mode = mode.gsub(/7(..)$/, 'rwx\1')
+  mode
+end
+
+def convert_group_right(mode)
+  mode = mode.gsub(/0(.)$/, '---\1')
+  mode = mode.gsub(/1(.)$/, '--x\1')
+  mode = mode.gsub(/2(.)$/, '-w-\1')
+  mode = mode.gsub(/3(.)$/, '-wx\1')
+  mode = mode.gsub(/4(.)$/, 'r--\1')
+  mode = mode.gsub(/5(.)$/, 'r-x\1')
+  mode = mode.gsub(/6(.)$/, 'rw-\1')
+  mode = mode.gsub(/7(.)$/, 'rwx\1')
+  mode
+end
+
+def convert_etc_right(mode)
+  mode = mode.gsub(/0$/, '---')
+  mode = mode.gsub(/1$/, '--x')
+  mode = mode.gsub(/2$/, '-w-')
+  mode = mode.gsub(/3$/, '-wx')
+  mode = mode.gsub(/4$/, 'r--')
+  mode = mode.gsub(/5$/, 'r-x')
+  mode = mode.gsub(/6$/, 'rw-')
+  mode = mode.gsub(/7$/, 'rwx')
 end
 
 def filter_directory_entries(argument)

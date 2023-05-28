90e87b4403e226e6212fd6116d1c52a2c7da8dd9
unikounio <bubbbble428@gmail.com>
2023-05-28 18:20:49 +0900

    処理の切り出し等による可読性の向上 - listメソッドの修正   - 変数名をformat_longに変更   - 変数entriesを削除し、使用していた箇所については代入していたメソッド呼び出し処理に書き換えた - 変数entry_informationsをentry_statusesに名称変更 - ブロックパラメータentry_informationをlong_entryに名称変更 - 可読性を上げるためにいくつかの処理をメソッドとして切り出し、あるいは共通化した   - display_long_entriesメソッドを追加   - shape_entry_nlinksメソッドを追加   - shape_entry_uidsメソッドを追加     - convert_uidsメソッドは統合のため削除   - shape_entry_gidsメソッドを追加     - convert_gidsメソッドは統合のため削除   - shape_entry_sizesメソッドを追加   - padding_informations_rjustメソッドを追加   - padding_informations_ljustメソッドを追加   - メソッド化に伴い配列entry_statusesに格納していた変数の一部を作成したメソッドと入れ替えた - エントリをlstatオブジェクト化した配列を変数entry_lstatsとして変数化した - convert_modeメソッドを削除し、呼び出し元であるdisplay_long_entriesに直接記載 - convert_datesメソッドをshape_entry_mtimesに名称変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -14,7 +14,7 @@ def main
   argument = ARGV[0] || '.'
 
   if option.include?('-l')
-    list(argument)
+    format_long(argument)
   elsif File.directory? argument
     display_directory_entries(argument)
   elsif File.file? argument
@@ -32,10 +32,9 @@ def define_option
   option
 end
 
-def list(argument)
+def format_long(argument)
   if File.directory? argument
-    entries = filter_directory_entries(argument)
-    entry_paths = entries.map { |entry| File.join(argument, entry) }
+    entry_paths = filter_directory_entries(argument).map { |entry| File.join(argument, entry) }
   elsif File.file? argument
     entry_paths = [argument]
   else
@@ -48,42 +47,60 @@ def list(argument)
     puts "合計 #{total_blocks}"
   end
 
-  entry_modes = entry_paths.map { |entry_path| convert_mode(File.lstat(entry_path)) }
+  display_long_entries(entry_paths)
+end
 
-  entry_nlinks = entry_paths.map { |entry_path| File.lstat(entry_path).nlink.to_s }
-  padded_entry_nlinks = entry_nlinks.map { |entry_nlink| entry_nlink.rjust(entry_nlinks.map(&:length).max) }
-
-  entry_uids = entry_paths.map { |entry_path| convert_uid(File.lstat(entry_path)) }
-  padded_entry_uids = entry_uids.map { |entry_uid| entry_uid.ljust(entry_uids.map(&:length).max) }
-
-  entry_gids = entry_paths.map { |entry_path| convert_gid(File.lstat(entry_path)) }
-  padded_entry_gids = entry_gids.map { |entry_gid| entry_gid.ljust(entry_gids.map(&:length).max) }
-
-  entry_sizes = entry_paths.map { |entry_path| File.lstat(entry_path).size.to_s }
-  padded_entry_sizes = entry_sizes.map { |entry_size| entry_size.rjust(entry_sizes.map(&:length).max) }
-
-  entry_dates = convert_dates(entry_paths)
-
+def display_long_entries(entry_paths)
+  entry_lstats = entry_paths.map { |entry_path| File.lstat(entry_path) }
+  entry_modes = entry_lstats.map do |entry_lstat|
+    entry_mode = convert_filetype(entry_lstat.mode.to_s(8))
+    convert_right(entry_mode)
+  end
   entry_basenames = entry_paths.map do |entry_path|
     File.symlink?(entry_path) ? "#{File.basename(entry_path)} -> #{File.readlink(entry_path)}" : File.basename(entry_path)
   end
+  entry_statuses = [entry_modes, shape_entry_nlinks(entry_lstats), shape_entry_uids(entry_lstats), shape_entry_gids(entry_lstats),
+                    shape_entry_sizes(entry_lstats), shape_entry_mtimes(entry_lstats), entry_basenames]
+  entry_statuses.transpose.each do |long_entry|
+    puts long_entry.join(' ')
+  end
+end
 
-  entry_infomations = [entry_modes, padded_entry_nlinks, padded_entry_uids, padded_entry_gids, padded_entry_sizes, entry_dates, entry_basenames]
+def shape_entry_nlinks(entry_lstats)
+  entry_nlinks = entry_lstats.map { |entry_lstat| entry_lstat.nlink.to_s }
+  padding_informations_rjust(entry_nlinks)
+end
 
-  entry_infomations.transpose.each do |entry_infomation|
-    puts entry_infomation.join(' ')
+def shape_entry_uids(entry_lstats)
+  entry_username = entry_lstats.map { |entry_lstat| Etc.getpwuid(entry_lstat.uid).name }
+  padding_informations_ljust(entry_username)
+end
+
+def shape_entry_gids(entry_lstats)
+  entry_groupname = entry_lstats.map { |entry_lstat| Etc.getgrgid(entry_lstat.gid).name }
+  padding_informations_ljust(entry_groupname)
+end
+
+def shape_entry_sizes(entry_lstats)
+  entry_sizes = entry_lstats.map { |entry_lstat| entry_lstat.size.to_s }
+  padding_informations_rjust(entry_sizes)
+end
+
+def padding_informations_rjust(entry_informations)
+  entry_informations.map do |entry_information|
+    entry_information.rjust(entry_informations.map(&:length).max)
+  end
+end
+
+def padding_informations_ljust(entry_informations)
+  entry_informations.map do |entry_information|
+    entry_information.ljust(entry_informations.map(&:length).max)
   end
 end
 
 def filter_directory_entries(argument)
   raw_entries = Dir.entries(argument).sort_by(&:downcase)
   raw_entries.reject { |entry| entry.start_with? '.' }
-end
-
-def convert_mode(entry_lstat)
-  mode = entry_lstat.mode.to_s(8)
-  mode = convert_filetype(mode)
-  convert_right(mode)
 end
 
 def convert_filetype(mode)
@@ -102,17 +119,6 @@ def convert_right(mode)
   mode = convert_group_right(mode)
   mode = convert_etc_right(mode)
   convert_special_right(mode)
-end
-
-def convert_special_right(mode)
-  if /^(.)1/.match?(mode)
-    mode = mode.gsub(/-$/, 'T')
-    mode = mode.gsub(/x$/, 't')
-  elsif /^(.)2/ =~ mode || /^(.)3/ =~ mode
-    mode = mode.gsub(/-$/, 'S')
-    mode = mode.gsub(/x$/, 's')
-  end
-  mode.gsub(/^(.)\d/, '\1')
 end
 
 def convert_user_right(mode)
@@ -148,18 +154,19 @@ def convert_etc_right(mode)
   mode.gsub(/7$/, 'rwx')
 end
 
-def convert_uid(entry_lstat)
-  user_id = entry_lstat.uid
-  Etc.getpwuid(user_id).name
+def convert_special_right(mode)
+  if /^(.)1/.match?(mode)
+    mode = mode.gsub(/-$/, 'T')
+    mode = mode.gsub(/x$/, 't')
+  elsif /^(.)2/ =~ mode || /^(.)3/ =~ mode
+    mode = mode.gsub(/-$/, 'S')
+    mode = mode.gsub(/x$/, 's')
+  end
+  mode.gsub(/^(.)\d/, '\1')
 end
 
-def convert_gid(entry_lstat)
-  group_id = entry_lstat.gid
-  Etc.getgrgid(group_id).name
-end
-
-def convert_dates(entry_paths)
-  entry_mtimes = entry_paths.map { |entry_path| File.lstat(entry_path).mtime }
+def shape_entry_mtimes(entry_lstats)
+  entry_mtimes = entry_lstats.map(&:mtime)
   months_and_days = entry_mtimes.map { |entry_mtime| entry_mtime.strftime('%b %e') }
   years_or_times = entry_mtimes.map do |entry_mtime|
     if Date.parse(entry_mtime.to_s) > Date.today << 6
@@ -168,7 +175,7 @@ def convert_dates(entry_paths)
       entry_mtime.strftime('%Y')
     end
   end
-  padded_entry_mtimes = [months_and_days, years_or_times.map { |years_or_time| years_or_time.rjust(years_or_times.map(&:length).max) }].transpose
+  padded_entry_mtimes = [months_and_days, padding_informations_rjust(years_or_times)].transpose
   padded_entry_mtimes.map { |padded_entry_mtime| padded_entry_mtime.join(' ') }
 end
 

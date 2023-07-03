bc67fdc59bdf9147c6d50effeb9b997336f34af8
unikounio <bubbbble428@gmail.com>
2023-07-04 00:37:07 +0900

    ディレクトリを指定した場合に対応

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,18 +1,23 @@
 def main
-  input_source = ARGV[0].nil? ? $stdin : File.open(ARGV[0])
-
   lines = 0
   words = 0
   characters = 0
 
-  input_source.each_line do |line|
-    lines += 1
-    words += line.split.size
-    characters += line.bytesize
+  if ARGV[0].nil? || File.file?(ARGV[0])
+    input_source = ARGV[0].nil? ? $stdin : File.open(ARGV[0])
+    input_source.each_line do |line|
+      lines += 1
+      words += line.split.size
+      characters += line.bytesize
+    end
+  else
+    puts "wc: #{ARGV[0]}: ディレクトリです"
   end
 
   print "#{lines}".rjust(7)
   print "#{words}".rjust(8)
-  puts"#{characters}".rjust(8)
+  puts "#{characters}".rjust(8)
+
 end
+
 main

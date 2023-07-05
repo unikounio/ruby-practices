e402139e338749e7bf80eaefc495add5bf2681e4
unikounio <bubbbble428@gmail.com>
2023-07-05 16:03:20 +0900

    オプション無しの機能が一通り完成 - ワイルドカード（*）を含む複数のファイルを指定できるようにした     - 引数を複数渡せるようにした     - 引数が複数の場合のみ合計欄がつくようにした     - 引数にワイルドカードが指定された場合それを展開できるようにした     - OSのwcコマンドに合わせて、単一ファイルが指定された場合と複数ファイルが指定された場合とで異なるレイアウトで出力されるようにした

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,29 +1,60 @@
 def main
-  lines = 0
-  words = 0
-  characters = 0
+  total_lines = 0
+  total_words = 0
+  total_characters = 0
 
-  if ARGV[0].nil? || File.file?(ARGV[0])
-    input_source = ARGV[0].nil? ? $stdin : File.open(ARGV[0])
-    input_source.each_line do |line|
+  input_sources = ARGV.empty? ? $stdin : Dir.glob(ARGV)
+  
+  if input_sources == $stdin
+    lines = 0
+    words = 0
+    characters = 0
+    input_sources.each_line do |line|
       lines += 1
       words += line.split.size
       characters += line.bytesize
     end
-  else
-    puts "wc: #{ARGV[0]}: ディレクトリです"
-  end
-
-  if ARGV[0].nil? || File.directory?(ARGV[0])
     print "#{lines}".rjust(7)
     print "#{words}".rjust(8)
     print "#{characters}".rjust(8)
-  else 
-    print " #{lines}  #{words} #{characters}"
-  end
+    puts ""
+  else
+    input_sources.each do |input_source|
+      lines = 0
+      words = 0
+      characters = 0
+      if File.file? input_source
+        File.open(input_source).each_line do |line|
+          lines += 1
+          words += line.split.size
+          characters += line.bytesize
+        end
+      else
+        puts "wc: #{input_source}: ディレクトリです"
+      end
+    
+      if ARGV.length == 1 && File.file?(input_source)
+        print " #{lines}  #{words} #{characters}"
+      else
+        print "#{lines}".rjust(7)
+        print "#{words}".rjust(8)
+        print "#{characters}".rjust(8)
+      end
+      print " #{input_source}"
+      puts ""
 
-  print " #{ARGV[0]}" unless ARGV[0].nil?
-  puts ""
+      total_lines += lines
+      total_words += words
+      total_characters += characters
+    end
+    
+    if ARGV.length > 1
+      print "#{total_lines}".rjust(7)
+      print "#{total_words}".rjust(8)
+      print "#{total_characters}".rjust(8)
+      puts ' 合計'
+    end
+  end
 end
 
 main

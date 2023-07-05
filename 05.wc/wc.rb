7e4c6cc93f16f0b2bfd553cfe3204cbcbb87cf85
unikounio <bubbbble428@gmail.com>
2023-07-05 19:49:51 +0900

    mainメソッド内の計算処理部分をcalculate_wcメソッドとして切り出した

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -8,34 +8,15 @@ def main
   total_words = 0
   total_characters = 0
 
-  input_sources = ARGV.empty? ? $stdin : Dir.glob(ARGV)
+  if ARGV.empty?
+    lines, words, characters = calculate_wc($stdin)
 
-  if input_sources == $stdin
-    lines = 0
-    words = 0
-    characters = 0
-    input_sources.each_line do |line|
-      lines += 1
-      words += line.split.size
-      characters += line.bytesize
-    end
     print_wc_results(options, lines, words, characters)
-
   else
-    input_sources.each do |input_source|
-      lines = 0
-      words = 0
-      characters = 0
-      if File.file? input_source
-        File.open(input_source).each_line do |line|
-          lines += 1
-          words += line.split.size
-          characters += line.bytesize
-        end
-      else
-        puts "wc: #{input_source}: Is a directory"
-      end
-      print_wc_results(options, lines, words, characters, input_source)
+    Dir.glob(ARGV).each do |argument|
+      lines, words, characters = calculate_wc(File.open(argument))
+
+      print_wc_results(options, lines, words, characters, argument)
       total_lines += lines
       total_words += words
       total_characters += characters
@@ -60,13 +41,29 @@ def define_options
   options
 end
 
-def print_wc_results(options, lines, words, characters, input_source = '')
+def calculate_wc(input_source)
+  lines = 0
+  words = 0
+  characters = 0
+
+  unless File.directory? input_source
+    input_source.each_line do |line|
+      lines += 1
+      words += line.split.size
+      characters += line.bytesize
+    end
+  end
+
+  [lines, words, characters]
+end
+
+def print_wc_results(options, lines, words, characters, argument = '')
+  puts "wc: #{argument}: Is a directory" if File.directory? argument
   print lines.to_s.rjust(7) if options.include?('-l') || options.empty?
   print words.to_s.rjust(8) if options.include?('-w') || options.empty?
   print characters.to_s.rjust(8) if options.include?('-c') || options.empty?
-  print ' ' + input_source
+  print " #{argument}"
   puts ''
 end
-
 
 main

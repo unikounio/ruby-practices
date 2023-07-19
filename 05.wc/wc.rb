509d936129423d9a07584c32db525458d2a9876b
unikounio <bubbbble428@gmail.com>
2023-07-19 23:47:23 +0900

    可読性・保守性の向上のためリファクタリング - 変数名の具体性を調整した - print_statistics（旧名print_lines_words_characters）メソッドにハッシュのまま引数を渡せるよう調整した - 引数の破壊的変更を行っていたcalculate_total_lines_words_charactersのメソッド化を解除した - File.openで開きっぱなしだった箇所をFile.readを使用するよう修正した - オプションパースに関する処理を簡潔化した

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -9,29 +9,33 @@ def main
   options = parse_options
 
   if ARGV.empty?
-    print_lines_words_characters_from_standard_input(options)
+    print_statistics_from_standard_input(options)
   else
-    print_lines_words_characters_and_total_from_argv(options)
+    print_statistics_and_total_from_argv(options)
   end
 end
 
 def parse_options
-  opt = OptionParser.new
-  options = []
-  opt.on('-l') { options << '-l' }
-  opt.on('-w') { options << '-w' }
-  opt.on('-c') { options << '-c' }
-  opt.parse!(ARGV)
+  options = {}
+
+  OptionParser.new do |opt|
+    opt.on('-l') { options[:lines] = true }
+    opt.on('-w') { options[:words] = true }
+    opt.on('-c') { options[:characters] = true }
+    opt.parse!(ARGV)
+  end
+
+  options = {lines: true, words: true, characters: true} if options.empty?
   options
 end
 
-def print_lines_words_characters_from_standard_input(options)
-  lines_words_characters = calculate_lines_words_characters($stdin)
-  print_lines_words_characters(lines_words_characters[:lines], lines_words_characters[:words], lines_words_characters[:characters], options)
+def print_statistics_from_standard_input(options)
+  statistics = calculate_statistics($stdin)
+  print_statistics(statistics, options)
   puts
 end
 
-def calculate_lines_words_characters(input_source)
+def calculate_statistics(input_source)
   lines = 0
   words = 0
   characters = 0
@@ -45,41 +49,36 @@ def calculate_lines_words_characters(input_source)
   { lines:, words:, characters: }
 end
 
-def print_lines_words_characters(lines, words, characters, options)
-  print lines.to_s.rjust(LINES_WIDTH) if options.include?('-l') || options.empty?
-  print words.to_s.rjust(WORDS_AND_CHARACTERS_WIDTH) if options.include?('-w') || options.empty?
-  print characters.to_s.rjust(WORDS_AND_CHARACTERS_WIDTH) if options.include?('-c') || options.empty?
+def print_statistics(statistics, options)
+  print statistics[:lines].to_s.rjust(LINES_WIDTH) if options[:lines]
+  print statistics[:words].to_s.rjust(WORDS_AND_CHARACTERS_WIDTH) if options[:words]
+  print statistics[:characters].to_s.rjust(WORDS_AND_CHARACTERS_WIDTH) if options[:characters]
 end
 
-def print_lines_words_characters_and_total_from_argv(options)
-  totals = { total_lines: 0, total_words: 0, total_characters: 0 }
+def print_statistics_and_total_from_argv(options)
+  totals = { lines: 0, words: 0, characters: 0 }
 
   Dir.glob(ARGV).each do |file_path|
-    lines_words_characters =
-      if File.directory? File.open(file_path)
+    statistics =
+      if File.directory? file_path
         { lines: 0, words: 0, characters: 0 }
       else
-        calculate_lines_words_characters(File.open(file_path))
+        calculate_statistics(File.read(file_path))
       end
 
     puts "wc: #{file_path}: Is a directory" if File.directory? file_path
-    print_lines_words_characters(lines_words_characters[:lines], lines_words_characters[:words], lines_words_characters[:characters], options)
+    print_statistics(statistics, options)
     puts " #{file_path}"
 
-    calculate_total_lines_words_characters(lines_words_characters, totals)
+    totals[:lines] += statistics[:lines]
+    totals[:words] += statistics[:words]
+    totals[:characters] += statistics[:characters]
   end
 
   return if ARGV.length == 1
 
-  print_lines_words_characters(totals[:total_lines], totals[:total_words], totals[:total_characters], options)
+  print_statistics(totals, options)
   puts ' total'
-end
-
-def calculate_total_lines_words_characters(lines_words_characters, totals)
-  totals[:total_lines] += lines_words_characters[:lines]
-  totals[:total_words] += lines_words_characters[:words]
-  totals[:total_characters] += lines_words_characters[:characters]
-  totals
 end
 
 main

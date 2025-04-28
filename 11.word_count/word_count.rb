961456587f4f47083c6064e522bbc5bb092baacf
unikounio <bubbbble428@gmail.com>
2025-04-28 12:12:10 +0900

    wordsの取得処理をリファクタリング 併せて不要な空行を削除した。

--- 11.word_count/word_count.rb
+++ 11.word_count/word_count.rb
@@ -4,13 +4,8 @@ Ruby is an interpreted, high-level, general-purpose programming language. It was
 Ruby is dynamically typed and uses garbage collection and just-in-time compilation. It supports multiple programming paradigms, including procedural, object-oriented, and functional programming. According to the creator, Ruby was influenced by Perl, Smalltalk, Eiffel, Ada, BASIC, Java, and Lisp.
 TEXT
 
-words = text.scan(/[A-Za-z0-9-]+/)
-lowercase_words = words.map do |word|
-  word.downcase
-end
-
-counted_words = lowercase_words.tally
-
+words = text.downcase.scan(/[\w-]+/)
+counted_words = words.tally
 sorted_words = counted_words.sort_by {|_, count| -count }
 
 sorted_words.each do |word, count|

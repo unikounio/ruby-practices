bbe259646064660395c57cabbdfa5e415cebadc3
unikounio <bubbbble428@gmail.com>
2025-04-30 11:38:47 +0900

    `<<`を使用していた箇所を`[*array, element]`の形に変更 以下の理由による。 - メソッドの戻り値としての意図が明確になる - 非破壊的に配列へ要素を追加できる

--- 12.row_col_sum/row_col_sum.rb
+++ 12.row_col_sum/row_col_sum.rb
@@ -1,13 +1,12 @@
 def row_col_sum(input)
   result = input.map do |row|
-    row_copy = row.clone
     row_total = row.sum
-    row_copy << row_total
+    [*row, row_total]
   end
 
   col_totals = result.transpose.map do |col|
     col.sum
   end
 
-  result << col_totals
+  [*result, col_totals]
 end

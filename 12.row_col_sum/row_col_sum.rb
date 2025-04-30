98e8c7fad9ad7184a1bee6c6df9fced371fc4004
unikounio <bubbbble428@gmail.com>
2025-04-30 11:20:12 +0900

    inputに破壊的変更を行わないようrow_col_sumメソッドを修正

--- 12.row_col_sum/row_col_sum.rb
+++ 12.row_col_sum/row_col_sum.rb
@@ -1,13 +1,14 @@
 def row_col_sum(input)
   result = input.map do |row|
+    row_copy = row.clone
     row_total = row.sum
-    row << row_total
+    row_copy << row_total
   end
-  
+
   col_totals = []
   result.transpose.each do |col|
     col_totals << col.sum
   end
-  
+
   result << col_totals
 end

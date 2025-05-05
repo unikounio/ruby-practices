5c6256e9cdd6e36180c9284a92a3a12b3a994f3b
unikounio <bubbbble428@gmail.com>
2025-05-05 10:58:43 +0900

    patternsを定数化

--- 13.keitai_message/keitai_message.rb
+++ 13.keitai_message/keitai_message.rb
@@ -1,23 +1,23 @@
+PATTERNS = {
+  1 => %w[' , ! ? \ ],
+  2 => %w[a b c],
+  3 => %w[d e f],
+  4 => %w[g h i],
+  5 => %w[j k l],
+  6 => %w[m n o],
+  7 => %w[p q r s],
+  8 => %w[t u v],
+  9 => %w[w x y z]
+}
+
 def keitai_message(input)
   testcase = input.split(/0+/)
   testcase.delete('')
 
-  patterns = {
-    1 => %w[' , ! ? \ ],
-    2 => %w[a b c],
-    3 => %w[d e f],
-    4 => %w[g h i],
-    5 => %w[j k l],
-    6 => %w[m n o],
-    7 => %w[p q r s],
-    8 => %w[t u v],
-    9 => %w[w x y z]
-  }
-
   result = ''
   testcase.each do |s|
     i = s[0].to_i
-    pattern = patterns[i]
+    pattern = PATTERNS[i]
     case i
     when 1
       if s.length % 5 == 0

ef0013ad03276dfba8f4689ab7658af5829489ae
unikounio <bubbbble428@gmail.com>
2025-05-12 15:57:24 +0900

    変数名を具体化

--- 14.serial_search/serial_search.rb
+++ 14.serial_search/serial_search.rb
@@ -1,14 +1,14 @@
 require 'date'
 
 def serial_search(input)
-  y = 1900 + (input[0] + input[4]).to_i
-  d = input[1..3].to_i
-  x = input[5..7].to_i
+  year = 1900 + (input[0] + input[4]).to_i
+  days = input[1..3].to_i
+  production = input[5..7].to_i
 
-  first_date = Date.new(y)
-  m_date = first_date + (d - 1)
+  first_date = Date.new(year)
+  manufactured = first_date + (days - 1)
 
-  f, n = (1..499).include?(x) ? ['カラマズー', x] : ['ナッシュビル', x - 500]
+  factory, order = (1..499).include?(production) ? ['カラマズー', production] : ['ナッシュビル', production - 500]
 
-  "#{m_date.year}年#{m_date.month}月#{m_date.day}日に#{f}・ファクトリーで#{n}番目に製造されたギターです"
+  "#{year}年#{manufactured.month}月#{manufactured.day}日に#{factory}・ファクトリーで#{order}番目に製造されたギターです"
 end

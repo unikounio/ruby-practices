a601e32998404342c87da7ef61ad8027df422f01
unikounio <bubbbble428@gmail.com>
2025-06-24 00:03:52 +0900

    変数名を再検討

--- 14.serial_search/serial_search.rb
+++ 14.serial_search/serial_search.rb
@@ -2,15 +2,17 @@
 
 require 'date'
 
+NASHVILLE_SERIAL_START = 500
+
 def serial_search(input)
   year = "19#{input[0]}#{input[4]})".to_i
   days = input[1..3].to_i
-  production = input[5..7].to_i
+  production_no = input[5..7].to_i
 
-  first_date = Date.new(year)
-  manufactured = first_date + (days - 1)
+  beginning_of_year = Date.new(year)
+  manufactured_on = beginning_of_year + days - 1
 
-  factory, order = (1..499).cover?(production) ? ['カラマズー', production] : ['ナッシュビル', production - 500]
+  factory, order = (1..499).cover?(production_no) ? ['カラマズー', production_no] : ['ナッシュビル', production_no - 500]
 
-  "#{year}年#{manufactured.month}月#{manufactured.day}日に#{factory}・ファクトリーで#{order}番目に製造されたギターです"
+  "#{year}年#{manufactured_on.month}月#{manufactured_on.day}日に#{factory}・ファクトリーで#{order}番目に製造されたギターです"
 end
